83dcb7c87912b947fab4316be0670129d047603b
ralphdg-dev <ralphkennethdg@gmail.com>
2025-02-12 11:30:33 +0800

    updated

---
+++ spotify-vite/src/components/Artist.js
@@ -0,0 +1,30 @@
+import "./MainContent.css";
+import bruno from "../assets/bruno.jpg";
+import arthur from "../assets/arthur.jpg";
+import lady from "../assets/lady.jpg";
+import taylor from "../assets/taylor.jpg";
+import sza from "../assets/sza.jpg";
+import gnx from "../assets/gnx.jpeg";
+import freudian from "../assets/freudian.jpg";
+import reputation from "../assets/reputation.jpg";
+import sweet from "../assets/sweet.jpeg";
+import kendrick from "../assets/kendrick.jpg"
+
+
+export const artists = [
+  { name: "Kendrick Lamar", img: kendrick},
+  { name: "Bruno Mars", img: bruno },
+  { name: "Arthur Nery", img: arthur },
+  { name: "Lady Gaga", img: lady },
+  { name: "Taylor Swift", img: taylor }
+];
+
+export const albums = [
+  { title: "SOS Deluxe: LANA", artist: "SZA", img: sza},
+  { title: "GNX", artist: "Kendrick Lamar", img: gnx},
+  { title: "Short n' Sweet", artist: "Sabrina Carpenter", img: sweet },
+  { title: "Reputation", artist: "Taylor Swift", img: reputation },
+  { title: "Freudian", artist: "Daniel Caesar", img: freudian }
+];
+
+
